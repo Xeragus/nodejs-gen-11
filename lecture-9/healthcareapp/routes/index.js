1b60ceef76246320c1b634aae420cdf1d8336462
Boban Sugareski <boban.s@slicelife.com>
2021-03-25 22:12:49 +0100

    Add doctors create & update routes

--- lecture-9/healthcareapp/routes/index.js
+++ lecture-9/healthcareapp/routes/index.js
@@ -14,11 +14,35 @@ router
     .get('/doctors/create', (req, res) => {
       res.render('doctors/create');
     })
-    .post('/doctors', async (req, res) => {
-      const doctor = new Doctor(req.body)
-      await doctor.save()
+    .get('/doctors/:id', async (req, res) => {
+      const doctor = await Doctor.findById(req.params.id)
 
-      res.redirect('/doctors')
+      res.render('doctors/update', doctor)
+    })
+    .post('/doctors', async (req, res) => {
+      try {
+        const doctor = new Doctor(req.body)
+        await doctor.save()
+
+        res.redirect('/doctors')
+      } catch (error) {
+        res.render('doctors/create', {
+          ...req.body,
+          error: error.message
+        })
+      }
+    })
+    .post('/doctors/:id', async (req, res) => {
+      try {
+        await Doctor.findByIdAndUpdate(req.params.id, req.body)
+
+        res.redirect('/doctors')
+      } catch (error) {
+        res.render('doctors/update', {
+          ...req.body,
+          error: error.message
+        })
+      }
     })
 
 module.exports = router;
